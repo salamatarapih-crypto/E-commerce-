d06b2fddb717bde0f61cf7daa2cae4d36897d410
salama moahamed-crypto <salamatarapih@gmail.com>
2026-09-21 22:36:01 +0300

    Remove unused useEffect

--- src/Cart/AllCart.js
+++ src/Cart/AllCart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import Navbar from "../Home/NavBar";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
